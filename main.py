b0a1134b43756675ac9832a08a67bdc71dc08f01
Mahesh-Chaulagain <maheshchaulagain8@gmail.com>
2023-11-08 05:23:52 +0545

    added option to add data to a file

--- main.py
+++ main.py
@@ -3,6 +3,18 @@ from tkinter import *
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+def save_data():
+    with open("data.txt", mode='a') as data_file:
+        website_data = website_entry.get()
+        email_data = email_entry.get()
+        password_data = password_entry.get()
+
+        data_file.write(f"{website_data} | {email_data} | {password_data} \n")
+
+        # Delete the entry data 
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -28,15 +40,19 @@ password_label.grid(column=0, row=3)
 # Entries
 website_entry = Entry(width=35)
 website_entry.grid(column=1, row=1, columnspan=2)
+# focus the cursor in the entry
+website_entry.focus()
 email_entry = Entry(width=35)
 email_entry.grid(column=1, row=2, columnspan=2)
+# populate field with commonly used email
+email_entry.insert(0, "example@email.com")
 password_entry = Entry(width=20)
 password_entry.grid(column=1, row=3)
 
 # Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(column=2, row=3)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save_data)
 add_button.grid(column=1, row=4, columnspan=2)
 
 window.mainloop()
